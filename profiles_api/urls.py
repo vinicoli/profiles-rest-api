3e1dd5e082b2109703d0a69108008923cd8418b0
Vinicius Oliveira <vinioliveira.web@gmail.com>
2023-02-23 18:32:37 -0300

    feat: :sparkles: add login API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -11,5 +11,6 @@ router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
-    path('', include(router.urls))
+    path('login', views.UserLoginAPIView.as_view()),
+    path('', include(router.urls)),
 ]
